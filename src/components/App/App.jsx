98185eb7f24d6ff39fb0496e230da66555ffc2e2
Clay Schurwon <clayschurwon@yahoo.com>
2024-10-28 12:46:37 -0500

    stage-1-2-api submission #3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "../Main/Main"; // Main page component
 import Footer from "../Footer/Footer"; // Footer component
 import SavedNews from "../SavedNews/SavedNews"; // Saved news page component
