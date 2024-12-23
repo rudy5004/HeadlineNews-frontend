f69c72e098892852e2204d700ed393811cf569b1
Clay Schurwon <clayschurwon@yahoo.com>
2024-12-22 20:34:08 -0600

    stage-1-2-api fixed BEM noncompliance with Preloader.jsx

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Preloader.css"; // Ensure you create this CSS file
 
 function Preloader() {
-  return <div className="circle-preloader"></div>;
+  return <div className="preloader preloader--circle"></div>;
 }
 
 export default Preloader;
